fb9f7ba02cdc4d6e68024051388dc7298e2c59f4
Nic0man <danic_169@hotmail.com>
2022-11-21 18:17:28 -0500

    v18:37

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom"
+
 export const Login = () => {
+
+    const navigate = useNavigate();
+    const handleOnclick = () => {
+        navigate("/register")
+    }
+
     return(
-        <div>
-            <button type="button" className="btn btn-primary">Login</button>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="col-md-12">
+                        <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
+                    </div>
+                    <div className="col-md-12">
+                        <input type="text" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1"></input>
+                    </div>    
+                    <div className="col-md-12">
+                        <button type="button" className="btn btn-primary" onClick={() => handleOnclick()}>Login</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
